f8d364502dce93172790201afafbdaacf93b1c41
Negar-ux <78379030+Negar-ux@users.noreply.github.com>
2021-04-23 22:57:45 -0400

    Ready to deploy

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_many :interviews
-  has_many :notes
+  has_many :notes 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
--- db/seeds.rb
+++ db/seeds.rb
@@ -14,24 +14,61 @@ Interview.destroy_all
 
 
 projects=Project.create! [
-                  {name: 'SEP759-Ruby on Rails' },
-                  {name: 'SEP700'},
-                  {name:'SEP761'},
-                  {name:'SEP760'},
+                  {name: 'Insights Lab' },
+                  {name:'Physical Fitness in Older Adults'},
                 ]
 
-users=User.create! [{
-                      email: 'basl@gmail.com',
-                    password: 'SEP759'}
+users=User.create! [
+                     { email: 'basl@gmail.com',
+                    password: 'SEP759'},
+                     { email: 'test@gmail.com',
+                       password: 'testtest'},
              ]
 
-interviews=Interview.create! [
+Interview.create! [
                     {title: 'Negar Deilami', date: '2021-03-30', description:'Engineering
-design student at McMaster University', project_id: projects[0].id, user_id: users[0].id}
+                    design student at McMaster University', project_id: projects[0].id, user_id: users[0].id},
+
+                    {title: 'Susan', date: '2021-02-16', description:'Susan has been retired for 3 years. She used to
+                    work in sales and now she volunteers as Patient/Family advisor at St. Joseph Hospital ',
+                     project_id: projects[1].id, user_id: users[1].id},
+
+                    {title: 'Mike', date: '2021-03-30', description:'Retired business owner',
+                    project_id: projects[1].id, user_id: users[1].id},
+
+                    {title: 'Abdel', date: '2021-03-30', description:'Semi-retied construction worker',
+                     project_id: projects[1].id, user_id: users[1].id},
+
+                    {title: 'Tess', date: '2021-03-30', description:'highschool teacher',
+                    project_id: projects[1].id, user_id: users[1].id},
+
+
+                    {title: 'Nilou', date: '2021-03-30', description:'Dietitian',
+                     project_id: projects[1].id, user_id: users[1].id},
                    ]
 
+#Negar
+Note.create!(insight: "Need an UX tool", user_id:User.first.id, interview_id: Interview.first.id)
+Note.create!(insight: "My user research is disorganized", user_id:User.first.id, interview_id: Interview.first.id)
 
-Note.create![
-  {insight: 'For my user research,I always lose track of my interviewees', user_id: users[0].id,
-   interview_id: interviews[0].id}
-]
+#Susan
+Note.create!(insight: "she had a high expectation of herself in exercising but after doing it she noticed that ", user_id:User.second.id, interview_id: Interview.second.id)
+Note.create!(insight: "Motivation is the biggest barrier to being fit", user_id:User.second.id, interview_id: Interview.second.id)
+
+
+#Mike
+Note.create!(insight: "Routine is what keeps him going", user_id:User.second.id, interview_id: Interview.third.id)
+Note.create!(insight: "Motivation is the biggest barrier to being fit", user_id:User.second.id, interview_id: Interview.third.id)
+
+#Abdel
+Note.create!(insight: "“I never think of myself as old, it is all about how you think”. He believes that
+how you think of your body has a strong influence on how active you are", user_id:User.second.id, interview_id: Interview.fourth.id)
+Note.create!(insight: "Motivation is the biggest barrier to being fit", user_id:User.second.id, interview_id: Interview.fourth.id)
+
+
+# Tess
+ Note.create!(insight: "socializing with the same group for a while
+seems more enjoyable than meeting with strangers frequently", user_id:User.second.id, interview_id: Interview.fifth.id)
+ Note.create!(insight: "“The more you stay inactive, the harder it is to start and turn it into a routine”",
+              user_id:User.second.id, interview_id: Interview.fifth.id)
+
